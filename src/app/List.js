f2df9557b3517cfe5de285f90cf2d7d2ab9f0b20
Antonio Jurado <antoniojuradoferreira@gmail.com>
2025-10-29 09:29:16 +0100

    Ejercicio1

--- src/app/List.js
+++ src/app/List.js
@@ -2,23 +2,49 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        conocido/a por {person.accomplishment}
-      </p>
-    </li>
+  const quimicos = people.filter(person =>
+    person.profession === 'químico'
   );
-  return (
+
+  const elresto = people.filter(person =>
+    person.profession !== 'químico'
+  );
+
+   return (
     <article>
       <h1>Científicos</h1>
-      <ul>{listItems}</ul>
+      <h2>Químicos</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              conocido/a por {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+      <h2>Todos los demás</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              conocido/a por {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
     </article>
   );
 }
