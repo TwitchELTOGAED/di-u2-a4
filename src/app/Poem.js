e691831f759341be845f94fbea8b03192389de6d
Antonio Jurado <antoniojuradoferreira@gmail.com>
2025-10-31 17:00:49 +0100

    Ejercicio4

--- src/app/Poem.js
+++ src/app/Poem.js
@@ -1,20 +1,27 @@
 const poem = {
-    lines: [
-      'Escribo, borro y reescribo',
-      'Borro de nuevo, y luego',
-      'Florece una amapola.'
-    ]
-  };
-  
-  export default function Poem() {
-    return (
-      <article>
-        {poem.lines.map((line, index) =>
-          <p key={index}>
-            {line}
-          </p>
-        )}
-      </article>
+  lines: [
+    'Escribo, borro y reescribo',
+    'Borro de nuevo, y luego',
+    'Florece una amapola.' 
+  ]
+};
+
+export default function Poem() {
+  let output = [];
+  poem.lines.forEach((line, i) => {
+    output.push(
+      <hr key={i + '-separator'} />
     );
-  }
-  
+    output.push(
+      <p key={i + '-text'}>
+        {line}
+      </p>
+    );
+  });
+  output.shift();
+  return (
+    <article>
+      {output}
+    </article>
+  );
+}
